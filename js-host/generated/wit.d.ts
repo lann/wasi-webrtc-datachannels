447adf12882e98c7d813e4d04006282738bc7bff
Lann Martin <github@lannbox.com>
2026-05-02 16:40:56 -0400

    Init

---
+++ js-host/generated/wit.d.ts
@@ -0,0 +1,2 @@
+// world wasi:webrtc-data-channels/imports@0.0.1
+export type * as WasiWebrtcDataChannelsTypes001 from './interfaces/wasi-webrtc-data-channels-types.js'; // import wasi:webrtc-data-channels/types@0.0.1
